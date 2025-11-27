d45209ad233c62b85f50f57fc6ad0e5ff2c39c1f
PajgerZs <pajgerzs@kkszki.hu>
2025-11-27 16:24:32 +0100

    Task 2 completed.

--- python1.py
+++ python1.py
@@ -2,3 +2,10 @@
 szam1 = int(input("Kérek egy egész számot: "))
 szam2 = int(input("Kérek még egy egész számot: "))
 print(szam1 + szam2)
+
+#2 Kérj be egy számot, és döntsd el, hogy páros-e.
+szam = int(input("Adj meg egy számot: "))
+if szam % 2 == 0:
+    print("A szám páros.")
+else:
+    print("A szám páratlan.")
