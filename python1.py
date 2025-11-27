25fdc9311a25d41f4c1df36402d9756747836f09
PajgerZs <pajgerzs@kkszki.hu>
2025-11-27 16:34:45 +0100

    Task 3  completed.

--- python1.py
+++ python1.py
@@ -1,6 +1,6 @@
 #1 Kérj be két számot, és írd ki az összegüket.
-szam1 = int(input("Kérek egy egész számot: "))
-szam2 = int(input("Kérek még egy egész számot: "))
+szam1 = float(input("Kérek egy számot: "))
+szam2 = float(input("Kérek még egy számot: "))
 print(szam1 + szam2)
 
 #2 Kérj be egy számot, és döntsd el, hogy páros-e.
@@ -9,3 +9,15 @@ if szam % 2 == 0:
     print("A szám páros.")
 else:
     print("A szám páratlan.")
+
+#3 Kérj be három számot, és írd ki melyik a legnagyobb.
+szam1 = int(input("Kérem az első számot: "))
+szam2 = int(input("Kérem a második számot: "))
+szam3 = int(input("Kérem a harmadik számot: "))
+legnagyobb = szam1
+if szam2 > legnagyobb:
+    legnagyobb = szam2
+if szam3 > legnagyobb:
+    legnagyobb = szam3
+
+print("A legnagyobb szám: ", legnagyobb)
